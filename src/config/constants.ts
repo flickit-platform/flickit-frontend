8a8c04adc4a97c52a1930ff6a217e63e7e3c5220
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-11-20 17:41:28 +0330

    OTAT-1869: use html instead of iframe

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,5 +2,9 @@ export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? import.meta.env.VITE_LOCAL_BASE_URL
     : process.env.BASE_URL;
-export const MULTILINGUALITY = import.meta.env.VITE_MULTILINGUALITY
+export const MULTILINGUALITY = import.meta.env.VITE_MULTILINGUALITY;
 export const APP_LABEL = import.meta.env.VITE_APP_TITLE;
+export const CDN_DIRECTORY =
+  process.env.NODE_ENV === "development"
+    ? "https://flickit-cdn.hectora.app/"
+    : import.meta.env.VITE_LOCAL_BASE_URL;
