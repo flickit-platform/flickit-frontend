c17ea14c9c7f75e0093b7ce71d150646dc42404f
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-12-25 11:36:44 +0330

    FRO-53: add new tab

---
+++ src/components/common/mainTabs/mainTabs.tsx
@@ -0,0 +1,97 @@
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { uniqueId } from "lodash";
+import { theme } from "@config/theme";
+import React from "react";
+import { Typography } from "@mui/material";
+import { Trans } from "react-i18next";
+
+const MainTabs = (props: any) => {
+  const { onTabChange, selectedTab } = props;
+
+  const tabListTitle = [
+    "dashboard",
+    "questions",
+    "insights",
+    "reportTitle",
+    "advices",
+    "settings",
+  ];
+
+  return (
+    <Box
+      sx={{
+        background: "#2466A814",
+        width: "100%",
+        borderRadius: "16px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        // boxShadow: "0 4px 4px rgba(0,0,0,25%)",
+        my: "50px",
+        paddingBlock: 1,
+        paddingInline: "48px",
+      }}
+    >
+      <Tabs
+        value={selectedTab}
+        onChange={(event, newValue) => onTabChange(event, newValue)}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        sx={{
+          border: "none",
+          width: "100%",
+          "& .MuiTabs-indicator": {
+            display: "none",
+          },
+        }}
+      >
+        {tabListTitle.map((title: any) => {
+          return (
+            <Tab
+              key={uniqueId()}
+              disabled={title != "dashboard"}
+              sx={{
+                ...theme.typography.semiBoldLarge,
+                flexGrow: 1,
+                mr: 1,
+                border: "none",
+                textTransform: "none",
+                paddingY: 1.5,
+                color: "#2B333B",
+                "&.Mui-selected": {
+                  boxShadow: "0 1px 4px rgba(0,0,0,25%) !important",
+                  borderRadius: 2,
+                  color: theme.palette.primary.main,
+                  background: "#fff",
+                  "&:hover": {
+                    background: "#fff",
+                    border: "none",
+                  },
+                },
+              }}
+              label={
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 1,
+                  }}
+                >
+                  <Typography variant="semiBoldLarge">
+                    <Trans i18nKey={title} />
+                  </Typography>
+                </Box>
+              }
+            />
+          );
+        })}
+      </Tabs>
+    </Box>
+  );
+};
+
+export default MainTabs;
--- src/components/dashboard/dashbordContainer.tsx
+++ src/components/dashboard/dashbordContainer.tsx
@@ -11,10 +11,11 @@ import PermissionControl from "@common/PermissionControl";
 import QueryBatchData from "@common/QueryBatchData";
 import LoadingSkeletonOfAssessmentRoles from "@common/loadings/LoadingSkeletonOfAssessmentRoles";
 import AssessmentHtmlTitle from "@components/assessment-html/AssessmentHtmlTitle";
-import {useQuery} from "@utils/useQuery";
-import {PathInfo} from "@types";
-import {useParams} from "react-router-dom";
+import { useQuery } from "@utils/useQuery";
+import { PathInfo } from "@types";
+import { useParams } from "react-router-dom";
 import DashboardTab from "@components/dashboard/dashboard_Tab/dashboardTab";
+import MainTabs from "@common/mainTabs/mainTabs";
 
 const DashbordContainer = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -25,180 +26,63 @@ const DashbordContainer = () => {
     setSelectedTab(newValue);
   };
 
-    const fetchPathInfo = useQuery<PathInfo>({
-        service: (args, config) =>
-            service.fetchPathInfo({ assessmentId, ...(args || {}) }, config),
-        runOnMount: true,
-    });
+  const fetchPathInfo = useQuery<PathInfo>({
+    service: (args, config) =>
+      service.fetchPathInfo({ assessmentId, ...(args || {}) }, config),
+    runOnMount: true,
+  });
 
   return (
-      // <PermissionControl error={[errorObject]}>
-          <QueryBatchData
-              queryBatchData={[fetchPathInfo]}
-              renderLoading={() => <LoadingSkeletonOfAssessmentRoles />}
-              render={([pathInfo]) => {
-                  return (
-                      <Box m="auto" pb={3} sx={{ px: { xl: 30, lg: 12, xs: 2, sm: 3 } }}>
-                          <DashboardTitle pathInfo={pathInfo} />
-                          <Grid container spacing={1} columns={12}>
-                              <Grid item sm={12} xs={12} mt={1}>
-                                  <Typography color="primary" textAlign="left" variant="headlineLarge">
-                                      <Trans i18nKey="dashboard" />
-                                  </Typography>
-                              </Grid>
-                              <Grid container sm={12} xs={12} mt={6}>
-                                  <Grid
-                                      item
-                                      // sm={3}
-                                      xs={12}
-                                      sx={{ display: "flex"}}
-                                  >
-                                      <Tabs
-                                          textColor="primary"
-                                          indicatorColor="primary"
-                                          orientation="horizontal"
-                                          variant="scrollable"
-                                          value={selectedTab}
-                                          onChange={handleTabChange}
-                                          aria-label="horizontal tabs"
-                                          centered={true}
-                                          sx={{
-                                              width:"100%",
-                                              borderRight: 1,
-                                              borderColor: "divider",
-                                              flexGrow: 1,
-                                              backgroundColor: "rgba(36, 102, 168, 0.04)",
-                                              padding: 0,
-                                              color: "rgba(0, 0, 0, 0.6)", // Default text color
+    // <PermissionControl error={[errorObject]}>
+    <QueryBatchData
+      queryBatchData={[fetchPathInfo]}
+      renderLoading={() => <LoadingSkeletonOfAssessmentRoles />}
+      render={([pathInfo]) => {
+        return (
+          <Box m="auto" pb={3} sx={{ px: { xl: 30, lg: 12, xs: 2, sm: 3 } }}>
+            <DashboardTitle pathInfo={pathInfo} />
+            <Grid container spacing={1} columns={12}>
+              <Grid item sm={12} xs={12} mt={1}>
+                <Typography
+                  color="primary"
+                  textAlign="left"
+                  variant="headlineLarge"
+                >
+                  <Trans i18nKey="dashboard" />
+                </Typography>
+              </Grid>
+              <Grid container sm={12} xs={12}>
+                <Grid
+                  item
+                  // sm={3}
+                  xs={12}
+                  sx={{ display: "flex" }}
+                >
+                  <MainTabs
+                    onTabChange={handleTabChange}
+                    selectedTab={selectedTab}
+                  />
+                </Grid>
 
-                                              // Adding hover state for background color and text color
-                                              "&:hover": {
-                                                  // backgroundColor: "#f0f0f0",
-                                                  // color: "#2466A8",
-                                              },
-                                              "& .Mui-selected": {
-                                                  color: "#2466A8 !important",
-                                                  fontWeight: "bold",
-                                                  // background: "rgba(36, 102, 168, 0.08)"
-                                              },
-
-                                              "& .MuiTabs-indicator": {
-                                                  backgroundColor: "primary.main",
-                                              },
-                                              "& .MuiTabs-flexContainer":{
-                                                  justifyContent:"space-between",
-                                              }
-                                          }}
-                                      >
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0",
-                                                  background:"#2466A805"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="Dashboard" />
-                                                  </Typography>
-                                              }
-                                          />
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="questions" />
-                                                  </Typography>
-                                              }
-                                          />
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="insights" />
-                                                  </Typography>
-                                              }
-                                          />
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="advices" />
-                                                  </Typography>
-                                              }
-                                          />
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="reports" />
-                                                  </Typography>
-                                              }
-                                          />{" "}
-                                          <Tab
-                                              sx={{
-                                                  alignItems: "center",
-                                                  textTransform: "none",
-                                                  flexGrow:1,
-                                                  border: "2px solid #C7CCD1",
-                                                  borderBottom: "none",
-                                                  borderRadius: ".5rem .5rem 0 0"
-                                              }}
-                                              label={
-                                                  <Typography variant="semiBoldLarge">
-                                                      <Trans i18nKey="settings" />
-                                                  </Typography>
-                                              }
-                                          />
-                                      </Tabs>
-                                  </Grid>
-
-                                  <Grid
-                                      item
-                                      // sm={9}
-                                      xs={12}
-                                      sx={{ height: "100%", background: "#F9FAFB",borderRadius: "0 0 1rem 1rem" }}
-                                  >
-                                      {selectedTab === 0 && <DashboardTab />}
-                                  </Grid>
-                              </Grid>
-                          </Grid>
-                      </Box>
-                  );
-              }}
-          />
-      // </PermissionControl>
+                <Grid
+                  item
+                  // sm={9}
+                  xs={12}
+                  sx={{
+                    height: "100%",
+                    background: "#F9FAFB",
+                    borderRadius: "1rem",
+                  }}
+                >
+                  {selectedTab === 0 && <DashboardTab />}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        );
+      }}
+    />
+    // </PermissionControl>
   );
 };
 export default DashbordContainer;
