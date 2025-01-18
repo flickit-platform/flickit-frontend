07964e45e593ec168e05d378bfdb31b648f29aef
mostafa.esmaeili <e.mostafa021@gmail.com>
2025-01-18 12:10:37 +0330

    FRO-145 : fix : remove box

--- src/components/common/errors/ErrorDataLoading.tsx
+++ src/components/common/errors/ErrorDataLoading.tsx
@@ -4,30 +4,23 @@ import { Typography } from "@mui/material";
 import { Trans } from "react-i18next";
 import { styles } from "@styles";
 import Button from "@mui/material/Button";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from "react";
 
 interface IErrorDataLoadingProps extends BoxProps {}
 
 const ErrorDataLoading = (props: IErrorDataLoadingProps) => {
   const { ...rest } = props;
-  return (<Box  sx={{ ...styles.centerCVH}}>
-          <Box  sx={{ ...styles.centerCVH }} pt="64px" pb="44px" {...rest}>
-              <ReportGmailerrorredRoundedIcon sx={{ fontSize: "4rem", mb: "16px" }} />
-              <Typography>
-                  <Trans i18nKey="someThingWentWrong" />
-              </Typography>
-          </Box>
-          <Button
-              variant="contained"
-              size="small"
-              component={Link}
-              to={"/"}
-          >
-              <Trans i18nKey={"backToHome"} />
-          </Button>
-  </Box>
-
+  return (
+    <Box sx={{ ...styles.centerCVH }} pt="64px" pb="44px" {...rest}>
+      <ReportGmailerrorredRoundedIcon sx={{ fontSize: "4rem", mb: "16px" }} />
+      <Typography>
+        <Trans i18nKey="someThingWentWrong" />
+      </Typography >
+      <Button sx={{mt:"50px"}} variant="contained" size="small" component={Link} to={"/"}>
+        <Trans i18nKey={"backToHome"} />
+      </Button>
+    </Box>
   );
 };
 
