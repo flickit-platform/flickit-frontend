79140c39c63ac3133fa4e855f00eef9695c229ec
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-01-20 13:23:27 +0330

    FRO-141: Fix: Fix logic for the same answer

--- src/components/questions/QuestionCard.tsx
+++ src/components/questions/QuestionCard.tsx
@@ -611,15 +611,24 @@ const AnswerTemplate = (props: {
   const navigate = useNavigate();
   const isLastQuestion = questionIndex == total_number_of_questions;
   const isSelectedValueTheSameAsAnswer =
-    (questionInfo?.answer?.selectedOption?.index == value?.index &&
-      questionInfo.answer?.confidenceLevel?.id === selcetedConfidenceLevel &&
-      questionInfo?.answer?.isNotApplicable === notApplicable) ||
-    (questionInfo?.mayNotBeApplicable &&
-      questionInfo?.answer?.isNotApplicable === notApplicable) ||
-    (questionInfo?.answer?.selectedOption == null &&
-      questionInfo.answer?.confidenceLevel == null &&
-      value === null &&
-      !questionInfo.mayNotBeApplicable);
+  (questionInfo?.answer?.selectedOption?.index === value?.index &&
+    questionInfo.answer?.confidenceLevel?.id === selcetedConfidenceLevel &&
+    questionInfo?.answer?.isNotApplicable === notApplicable) ||
+  (questionInfo?.mayNotBeApplicable &&
+    questionInfo?.answer?.isNotApplicable === true &&
+    notApplicable === true) ||
+  (questionInfo?.answer === null &&
+    value === null &&
+    notApplicable === false) ||
+  (questionInfo?.answer?.selectedOption === null &&
+    questionInfo.answer?.confidenceLevel === null &&
+    value === null &&
+    questionInfo?.answer?.isNotApplicable === notApplicable) ||
+  (questionInfo?.answer?.selectedOption === null &&
+    questionInfo.answer?.confidenceLevel === null &&
+    value === null &&
+    !questionInfo.mayNotBeApplicable);
+
 
   const changeHappened = useRef(false);
   const onChange = (
