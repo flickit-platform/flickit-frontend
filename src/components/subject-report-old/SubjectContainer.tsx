6dc39239ef9cf3bb76df861b7f41068f2290ee8f
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-04-08 21:51:21 +0330

    FRO-262: remove unused imports based on sonar analytics

--- src/components/subject-report-old/SubjectContainer.tsx
+++ src/components/subject-report-old/SubjectContainer.tsx
@@ -1,13 +1,6 @@
-import { useEffect } from "react";
 import Box from "@mui/material/Box";
-import { useParams } from "react-router-dom";
-import GettingThingsReadyLoading from "@common/loadings/GettingThingsReadyLoading";
-import { styles } from "@styles";
-import { useServiceContext } from "@providers/ServiceProvider";
-import { useQuery } from "@utils/useQuery";
 import { SubjectAttributeList } from "./SubjectAttributeList";
 import SubjectOverallInsight from "./SubjectOverallInsight";
-import QueryBatchData from "@common/QueryBatchData";
 
 const SubjectContainer = (props: any) => {
   const { id, reloadQuery, progress } = props;
