74d77aa7e329e086b344601e10ee0c33f124f33c
e.mostafa <e.mostafa021@gmail.com>
2025-04-08 12:26:30 +0330

    FRO-270 : fix : fix persian questionnaire kit designer

--- src/components/kit-designer/questionnaires/QuestionnaireList.tsx
+++ src/components/kit-designer/questionnaires/QuestionnaireList.tsx
@@ -12,7 +12,7 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { styles } from "@styles";
 import { KitDesignListItems, TId } from "@types";
 import { Trans } from "react-i18next";
-import { theme } from "@config/theme";
+import {farsiFontFamily, primaryFontFamily, theme} from "@config/theme";
 import languageDetector from "@utils/languageDetector";
 import QuestionContainer from "@components/kit-designer/questionnaires/questions/QuestionContainer";
 import Accordion from "@mui/material/Accordion";
@@ -399,6 +399,7 @@ const ListOfItems = ({
                                   onChange={(e) => handelChange(e)}
                                   inputProps={{
                                     "data-testid": "items-title",
+                                    style: { fontFamily: languageDetector(tempValues.title) ? farsiFontFamily : primaryFontFamily }
                                   }}
                                   variant="outlined"
                                   fullWidth
@@ -423,7 +424,7 @@ const ListOfItems = ({
                               ) : (
                                 <Typography
                                   variant="h6"
-                                  sx={{ flexGrow: 1, width: "80%" }}
+                                  sx={{ flexGrow: 1, width: "80%", fontFamily: languageDetector(item.title) ? farsiFontFamily : primaryFontFamily }}
                                 >
                                   {item.title}
                                 </Typography>
@@ -510,6 +511,7 @@ const ListOfItems = ({
                                   name="description"
                                   inputProps={{
                                     "data-testid": "items-description",
+                                    style: { fontFamily: languageDetector(tempValues.description) ? farsiFontFamily : primaryFontFamily }
                                   }}
                                   variant="outlined"
                                   fullWidth
@@ -539,11 +541,7 @@ const ListOfItems = ({
                                 <Typography
                                   sx={{
                                     wordBreak: "break-word",
-                                    textAlign: languageDetector(
-                                      item.description,
-                                    )
-                                      ? "right"
-                                      : "left",
+                                    fontFamily: languageDetector(item.description) ? farsiFontFamily : primaryFontFamily,
                                     width: "80%",
                                   }}
                                   variant="body2"
