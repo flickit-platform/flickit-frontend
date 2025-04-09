72fca87436ea3ad09ea6c36715f0d8a2062b0771
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-04-09 14:47:11 +0330

    FRO-270: fix title font

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -7,7 +7,7 @@ import { SvgIconProps } from "@mui/material/SvgIcon";
 import AnchorRoundedIcon from "@mui/icons-material/AnchorRounded";
 import { styles } from "@styles";
 import HomeIcon from "@mui/icons-material/Home";
-import {farsiFontFamily, primaryFontFamily, theme} from "@/config/theme";
+import { farsiFontFamily, primaryFontFamily, theme } from "@/config/theme";
 import languageDetector from "@utils/languageDetector";
 interface ITitle extends Omit<TypographyProps, "borderBottom"> {
   sup?: JSX.Element | string;
@@ -146,9 +146,7 @@ const Title = (props: ITitle) => {
           sx={{
             ...styles.centerV,
             display: { xs: "block", sm: "flex" },
-            fontFamily: languageDetector(children as string)
-                ? farsiFontFamily
-                : primaryFontFamily,
+
             ...((titleProps?.sx ?? {}) as any),
             ...(rest.sx ?? {}),
           }}
