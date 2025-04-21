16b72220c5dfbf44e421006ac947891c15da5803
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-04-21 09:29:20 +0330

    FRO-331: undo trimming values

--- src/providers/FlagsProvider/actions.tsx
+++
@@ -1,8 +0,0 @@
-export const SET_SHOW_BUTTON = "SET_SHOW_BUTTON";
-
-interface SetShowButtonAction {
-  type: typeof SET_SHOW_BUTTON;
-  payload: boolean;
-}
-
-export type FlagsAction = SetShowButtonAction;
