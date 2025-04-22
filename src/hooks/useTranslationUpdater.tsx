eb792111677752d98517364fb12a38c93e040657
e.mostafa <e.mostafa021@gmail.com>
2025-04-22 15:12:25 +0330

    FRO-305: fix : fix translations

--- src/hooks/useTranslationUpdater.tsx
+++ src/hooks/useTranslationUpdater.tsx
@@ -10,14 +10,19 @@ export const useTranslationUpdater = (langCode?: string) => {
 
     setState((prev: any) => {
       const updatedTranslation =
-            {
+         value === "" ?
+           {
+             ...prev.translations?.[langCode],
+             [field]: undefined,
+           }
+            :
+           {
               ...prev.translations?.[langCode],
               [field]: value,
             };
-
       return {
         ...prev,
-        translations: updatedTranslation
+        translations: !Object.values(updatedTranslation).every(item => item === undefined)
           ? {
               ...prev.translations,
               [langCode]: updatedTranslation,
