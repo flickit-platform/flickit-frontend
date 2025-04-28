6412071ef5c03bd71003a6dc73cf96e65624ae96
e.mostafa <e.mostafa021@gmail.com>
2025-04-28 17:40:54 +0330

    FRO-350: feat : kit store responsive

--- src/components/assessment-kit/AssessmentKitsContactUs.tsx
+++ src/components/assessment-kit/AssessmentKitsContactUs.tsx
@@ -1,54 +1,76 @@
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import FaContactUs from "@/assets/svg/fa-contactus.svg";
-import EnContactUs from "@/assets/svg/en-contactus.svg";
 import useDialog from "@/utils/useDialog";
-import ContactUsDialog from "./ContactUsDialog";
-import i18next, { t } from "i18next";
+import { t } from "i18next";
 import { useMediaQuery } from "@mui/material";
-import { theme } from "@/config/theme";
+import contactUs from "@/assets/svg/contactUs.svg";
+import Typography from "@mui/material/Typography";
+import { theme } from "@config/theme";
+import Button from "@mui/material/Button";
 
 const AssessmentKitsContactUs = () => {
   const dialogProps = useDialog();
   const isMobileScreen = useMediaQuery((theme: any) =>
-    theme.breakpoints.down(960),
+    theme.breakpoints.down("sm"),
   );
 
   return (
     <Box
       sx={{
-        position: "relative",
+        display: "flex",
+        flexDirection: {xs: "column", sm: "row-reverse"},
+        justifyContent: "space-between",
         width: "100%",
+        background: "#D5E5F6",
+        borderRadius: 2,
+        px: 4,
+        pb: {xs: 4,sm: "unset"}
       }}
     >
       <Box
-        component="img"
-        src={i18next.language === "fa" ? FaContactUs : EnContactUs}
-        alt="Contact Us"
-        sx={{ width: "100%", display: "block" }}
-        onClick={() => dialogProps.openDialog({})}
-      />
-
-      <Button
-        variant="contained"
-        onClick={() => dialogProps.openDialog({})}
-        fullWidth={isMobileScreen}
-        sx={{
-          mt: { xs: 0, md: 0 },
-          position: { sm: "static", md: "absolute" },
-          bottom: { sm: "62px" },
-          [theme.direction === "rtl" ? "right" : "left"]: { sm: "3.8%" },
-          alignSelf: { sm: "center", md: "flex-start" },
-          backgroundColor: "primary.main",
-          "&:hover": {
-            backgroundColor: "primary.dark",
-          },
-        }}
-        size="large"
+      component={"img"}
+      src={contactUs}
+      alt="Contact Us"
+      sx={{ width: {xs: "100%", md: "495px" }, height: {xs: "256px", md: "291px"} }}
+      onClick={() => dialogProps.openDialog({})}
       >
-        {t("contactUs")}
-      </Button>
-      <ContactUsDialog {...dialogProps} />
+      </Box>
+      <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+        <Typography
+          sx={{
+            ...theme.typography.headlineMedium,
+            color: "#2B333B",
+            maxWidth: { xs: "80%", sm: "unset" } ,
+            mb: { xs: "12px", sm: 4 },
+          }}
+        >
+          {t("lookingForSomethingSpecific")}
+        </Typography>
+        <Typography
+          sx={{
+            ...theme.typography.semiBoldXLarge,
+            color: "#6C8093",
+            mb: { xs: 2, sm: 4 },
+          }}
+        >
+          {t("dropUsNote")}
+        </Typography>
+           <Button
+             variant="contained"
+             onClick={() => dialogProps.openDialog({})}
+             fullWidth={isMobileScreen}
+             sx={{
+               mt: { xs: 0, md: 0 },
+               alignSelf: "flex-start",
+               backgroundColor: "primary.main",
+               "&:hover": {
+                 backgroundColor: "primary.dark",
+               },
+             }}
+             size="large"
+           >
+             {t("contactUs")}
+           </Button>
+      </Box>
     </Box>
   );
 };
