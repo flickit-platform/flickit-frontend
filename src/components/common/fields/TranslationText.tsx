30d289c8868e3921ce5bbff501fcf19d93595393
mostafa.esmaeili <e.mostafa021@gmail.com>
2025-05-04 01:15:14 +0330

    FRO-297 : fix : fix issues

--- src/components/common/fields/TranslationText.tsx
+++ src/components/common/fields/TranslationText.tsx
@@ -1,10 +1,9 @@
 import { Box, Typography } from "@mui/material";
-import { farsiFontFamily, primaryFontFamily } from "@/config/theme";
+import { farsiFontFamily, primaryFontFamily, theme } from "@/config/theme";
 import languageDetector from "@/utils/languageDetector";
 import { useState } from "react";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { toast } from "react-toastify";
-import { theme } from "@/config/theme";
 import Tooltip from "@mui/material/Tooltip";
 
 interface TitleWithTranslationProps {
@@ -12,82 +11,87 @@ interface TitleWithTranslationProps {
   translation?: string;
   variant?: any;
   multiline?: boolean;
-  showCopyIcon?: boolean | undefined;
+  showCopyIcon?: boolean;
 }
 
 const TitleWithTranslation = ({
   title,
   translation,
-  variant = "h6",
-  multiline = false,
-  showCopyIcon= false
+ ...rest
 }: TitleWithTranslationProps) => {
   const isFarsiTitle = languageDetector(title);
   const isFarsiTranslation = translation
     ? languageDetector(translation)
     : false;
 
-  const RenderText = ({
-    text,
-    isFarsi,
-    color,
-    variantOverride,
-  }: {
-    text: string;
-    isFarsi: boolean;
-    color?: string;
-    variantOverride?: any;
-  }) => {
-    const baseProps = {
-      component: "div" as const,
-      variant: variantOverride ?? variant,
-      sx: {
-        mt: !multiline ? 0.5 : 0,
-        color: color ?? "inherit",
-        fontFamily: isFarsi ? farsiFontFamily : primaryFontFamily,
-        textAlign: multiline ? "justify" : "unset",
-        width: "fit-content",
-      },
-    };
-    const [show,setShow] = useState(false)
+  return (
+    <Box display="flex" flexDirection="column" flexGrow={1}>
+      <RenderText text={title} isFarsi={isFarsiTitle}  {...rest} />
+      {translation &&
+        <RenderText text={translation} isFarsi={isFarsiTranslation} color={"#6C8093"} variantOverride={"body2"}
+            {...rest} />}
+    </Box>
+  );
+};
 
-    const handleMouseOver = () => {
-     setShow(true)
-    };
-    const handelMouseOut = () => {
-      setShow(false)
-    };
-    const handleCopyClick = () => {
-      navigator.clipboard.writeText(text).then(() => {
-        toast("copied",{type: 'success'})
-      });
-    };
+const RenderText = ({
+                      text,
+                      isFarsi,
+                      color,
+                      variantOverride,
+                      variant,
+                      multiline,
+                      showCopyIcon
+                    }: {
+  text: string;
+  isFarsi: boolean;
+  color?: string;
+  variantOverride?: any;
+  variant?: string,
+  multiline?: boolean,
+  showCopyIcon?: boolean
+}) => {
+  const baseProps = {
+    component: "div" as const,
+    variant: variantOverride ?? variant,
+    sx: {
+      mt: !multiline ? 0.5 : 0,
+      color: color ?? "inherit",
+      fontFamily: isFarsi ? farsiFontFamily : primaryFontFamily,
+      textAlign: multiline ? "justify" : "unset",
+      width: "fit-content",
+    },
+  };
+  const [show,setShow] = useState(false)
 
-    return multiline ? (
+  const handleMouseOver = () => {
+    setShow(true)
+  };
+  const handelMouseOut = () => {
+    setShow(false)
+  };
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(text).then(() => {
+      toast("copied",{type: 'success'})
+    });
+  };
+
+  return multiline ? (
       <Typography {...baseProps} dangerouslySetInnerHTML={{ __html: text }} />
-    ) : (
+  ) : (
       <Typography
-        onClick={handleCopyClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handelMouseOut}
-        {...baseProps}
+          onClick={handleCopyClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handelMouseOut}
+          {...baseProps}
       >
         {text}{" "}
         {show && showCopyIcon && (
-          <Tooltip title={"copy"} >
-            <ContentCopyIcon sx={{ float:[theme.direction == "rtl" ? "left" :  "right"], marginInlineStart: "10px" }} fontSize={"small"} />
-          </Tooltip>
+            <Tooltip title={"copy"} >
+              <ContentCopyIcon sx={{ float:[theme.direction == "rtl" ? "left" :  "right"], marginInlineStart: "10px" }} fontSize={"small"} />
+            </Tooltip>
         )}
       </Typography>
-    );
-  };
-
-  return (
-    <Box display="flex" flexDirection="column" flexGrow={1}>
-      <RenderText text={title} isFarsi={isFarsiTitle} />
-      {translation &&
-        <RenderText text={translation} isFarsi={isFarsiTranslation} color={"#6C8093"} variantOverride={"body2"} />}
-    </Box>
   );
 };
 
