b83c2a1fe95ae989464be70c8fa4ee36a7104b77
e.mostafa <e.mostafa021@gmail.com>
2025-05-04 14:24:58 +0330

    FRO-359: feat : remove wapper

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,14 +3,28 @@ import Box from "@mui/material/Box";
 import GettingThingsReadyLoading from "@common/loadings/GettingThingsReadyLoading";
 import Navbar from "@common/Navbar";
 import { styles } from "@styles";
-import WrapperApp from "@utils/wrapperApp";
+import { useLocation, useParams } from "react-router-dom";
 
 const AppLayout = (props: PropsWithChildren<{}>) => {
+
   const { children } = props;
+  const { pathname } = useLocation()
+  const { assessmentKitId = "" } = useParams();
+
+  const checkLink = (link: string)=>{
+    return  pathname.startsWith(link)
+  }
+
   return (
     <Box sx={{ overflowX: "clip", minHeight: "100vh" }}>
       <Navbar />
       <Box
+        sx={{
+          p: !checkLink("/assessment-kits")
+            ? { xs: 1, sm: 1, md: 4 }
+            : "0",
+          px: checkLink(`/assessment-kits/${assessmentKitId}`) ?  0 :  { xl: 30, lg: 12, xs: 2, sm: 3 }
+        }}
         m="auto"
       >
         <Suspense
@@ -20,9 +34,7 @@ const AppLayout = (props: PropsWithChildren<{}>) => {
             </Box>
           }
         >
-          <WrapperApp>
-            {children}
-          </WrapperApp>
+         {children}
         </Suspense>
       </Box>
     </Box>
