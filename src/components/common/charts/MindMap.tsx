ade524f48da4b982ae59cb443b0d9b5444329ff2
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-05-05 14:31:05 +0330

    FRO-373: remove box shadow

--- src/components/common/charts/MindMap.tsx
+++ src/components/common/charts/MindMap.tsx
@@ -277,7 +277,6 @@ const MindMap: React.FC<MindMapProps> = ({
                         fontSize: attributeFontSize,
                         gap: "4px",
                         minWidth: "100px",
-                        boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
                       }}
                     >
                       <Typography
