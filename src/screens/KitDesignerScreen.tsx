84988495214f2989e5268ff5ea0deadf057283da
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-05-06 13:44:04 +0330

    FRO-362: remove unused imports

--- src/screens/KitDesignerScreen.tsx
+++ src/screens/KitDesignerScreen.tsx
@@ -1,7 +1,6 @@
 import { t } from "i18next";
 import useDocumentTitle from "@utils/useDocumentTitle";
 import KitDesignerContainer from "@/components/kit-designer/KitDesignerContainer";
-import { KitLanguageProvider } from "@/providers/KitProvider";
 
 const KitDesignerScreen = () => {
   useDocumentTitle(t("kitDesigner") as string);
