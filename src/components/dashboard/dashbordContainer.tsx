1bf92544a9fa1dcb5801368167d528f14efc7ae3
e.mostafa <e.mostafa021@gmail.com>
2025-05-10 14:04:29 +0330

    FRO-363: fix : new design for tab menu

--- src/components/dashboard/dashbordContainer.tsx
+++ src/components/dashboard/dashbordContainer.tsx
@@ -8,10 +8,42 @@ import QueryBatchData from "@common/QueryBatchData";
 import LoadingSkeletonOfAssessmentRoles from "@common/loadings/LoadingSkeletonOfAssessmentRoles";
 import { useQuery } from "@utils/useQuery";
 import { PathInfo } from "@/types/index";
-import { useLocation, useOutlet, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useOutlet, useParams } from "react-router-dom";
 import MainTabs from "@/components/dashboard/MainTabs";
 import languageDetector from "@/utils/languageDetector";
 import { farsiFontFamily, primaryFontFamily } from "@/config/theme";
+import useScreenResize from "@utils/useScreenResize";
+import { styles } from "@styles";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { theme } from "@/config/theme";
+import { Trans } from "react-i18next";
+
+const tabListTitle = [
+  { label: "dashboard", address: "dashboard", permission: "viewDashboard" },
+  {
+    label: "questions",
+    address: "questionnaires",
+    permission: "viewAssessmentQuestionnaireList",
+  },
+  {
+    label: "insights",
+    address: "insights",
+    permission: "viewAssessmentInsights",
+  },
+  { label: "advice", address: "advice", permission: "createAdvice" },
+  {
+    label: "reportTitle",
+    address: "report",
+    permission: "manageReportMetadata",
+  },
+  {
+    label: "settings",
+    address: "settings",
+    permission: "grantUserAssessmentRole",
+  },
+];
 
 const DashbordContainer = () => {
   const location = useLocation();
@@ -19,7 +51,7 @@ const DashbordContainer = () => {
   const { service } = useServiceContext();
   const { assessmentId = "" } = useParams();
   const outlet = useOutlet();
-
+  const navigate = useNavigate()
   useEffect(() => {
     const pathSegments = location.pathname
       .split("/")
@@ -36,6 +68,20 @@ const DashbordContainer = () => {
       service.common.getPathInfo({ assessmentId, ...(args ?? {}) }, config),
     runOnMount: true,
   });
+
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const { value } = event.target;
+    setSelectedTab(value as string);
+
+    const selectedItem = tabListTitle.find((item) => item.address === value);
+    if (selectedItem) {
+      const { address } = selectedItem;
+      navigate(`./${address}/`);
+    }
+  };
+
+  const isMobileScreen = useScreenResize("sm")
 
   return (
     <QueryBatchData
@@ -75,10 +121,97 @@ const DashbordContainer = () => {
                 xs={Number(pathInfo?.assessment?.title?.length) < 20 ? 8 : 12}
                 sx={{ display: "flex" }}
               >
-                <MainTabs
-                  onTabChange={handleTabChange}
-                  selectedTab={selectedTab}
-                />
+                {isMobileScreen ? (
+                  <Box
+                    sx={{
+                      ...styles.centerVH,
+                      mt: 1,
+                      background: "#2466A814",
+                      width: "70%",
+                      borderRadius: "16px",
+                      p: 1.3,
+                    }}
+                  >
+                    <FormControl fullWidth>
+                      <Select
+                        value={selectedTab}
+                        onChange={(event: any) => handleChange(event)}
+                        MenuProps={{
+                          PaperProps: {
+                            sx: {
+                              mt: 0,
+                              borderTop: "none",
+                              borderRadius: "0 0 8px 8px",
+                              boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+                            },
+                          },
+                          MenuListProps: {
+                            disablePadding: true,
+                          },
+                        }}
+                        sx={{
+                          height: "40px",
+                          background: "#fff",
+                          width: { xs: "100%" },
+                          "& .MuiOutlinedInput-notchedOutline": {
+                            border: "none",
+                          },
+                          "& .MuiOutlinedInput-root": {
+                            borderRadius: "8px 8px 0 0",
+                            boxShadow: "0 1px 4px rgba(0,0,0,0.25)!important",
+                          },
+                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            border: "none",
+                          },
+                          "& .MuiOutlinedInput-root.Mui-focused": {
+                            boxShadow: "none",
+                          },
+                          "& .MuiTypography-root": {
+                            ...theme.typography.semiBoldMedium,
+                          },
+                          color: theme.palette.primary.main,
+                        }}
+                      >
+                        {tabListTitle.map((item) => {
+                          const { label, address } = item;
+                          return (
+                            <MenuItem
+                              key={label}
+                              value={address}
+                              sx={{
+                                fontFamily: languageDetector(label)
+                                  ? farsiFontFamily
+                                  : primaryFontFamily,
+                                ...theme.typography.semiBoldMedium,
+                                fontSize: "14px",
+                                transition: "all 0.2s ease",
+                                "&:hover": {
+                                  backgroundColor: "#2466A830",
+                                  color: theme.palette.primary.main,
+                                  fontWeight: 600,
+                                },
+                                "&.Mui-selected": {
+                                  backgroundColor: "#2466A820 !important",
+                                },
+                                "&.Mui-selected:hover": {
+                                  backgroundColor: "#2466A840 !important",
+                                },
+                              }}
+                            >
+                              <Trans i18nKey={label} />
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </FormControl>
+                  </Box>
+                ) : (
+                  <MainTabs
+                    onTabChange={handleTabChange}
+                    selectedTab={selectedTab}
+                    tabListTitle={tabListTitle}
+                  />
+                )}
               </Grid>
               <Grid container>
                 <Grid item xs={12}>
