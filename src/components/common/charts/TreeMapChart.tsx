47bb59156d89824676e31a7fd121a55ca20667ac
e.mostafa <e.mostafa021@gmail.com>
2025-05-12 13:09:21 +0330

    FRO-392: fix :fix chart description

--- src/components/common/charts/TreeMapChart.tsx
+++ src/components/common/charts/TreeMapChart.tsx
@@ -4,6 +4,7 @@ import { getMaturityLevelColors } from "@styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { theme } from "@/config/theme";
 import languageDetector from "@/utils/languageDetector";
+import ChartTooltip from "@/components/common/charts/ChartTooltip"
 
 interface TreeMapNode {
   name: string;
@@ -43,7 +44,7 @@ const TreeMapChart: React.FC<TreeMapProps> = ({ data, levels }) => {
       >
         <Tooltip
           wrapperStyle={{ outline: "none" }}
-          content={<CustomTooltip />}
+          content={<ChartTooltip />}
         />
       </Treemap>
     </ResponsiveContainer>
@@ -94,31 +95,6 @@ const CustomNode: any = (props: any) => {
       )}
     </g>
   );
-};
-
-const CustomTooltip = ({ active, payload }: any) => {
-  if (active && payload?.length) {
-    const { description } = payload[0].payload;
-    return (
-      <div
-        style={{
-          backgroundColor: "rgba(97, 97, 97, 0.92)",
-          borderRadius: "4px",
-          color: "white",
-          maxWidth: "300px",
-          overflowWrap: "break-word",
-          fontWeight: 500,
-          border: "none !important",
-          boxShadow: "none",
-          fontSize: 12,
-          padding: 3,
-        }}
-      >
-        <p>{description}</p>
-      </div>
-    );
-  }
-  return null;
 };
 
 export default TreeMapChart;
