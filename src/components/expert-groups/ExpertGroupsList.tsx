4b84d8b0ae7616957bb577ab37e2052bc8c4fcb7
e.mostafa <e.mostafa021@gmail.com>
2025-08-04 16:53:09 +0330

    fro-542 : add confirmation dialog to delete expert groups

--- src/components/expert-groups/ExpertGroupsList.tsx
+++ src/components/expert-groups/ExpertGroupsList.tsx
@@ -1,6 +1,15 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import ExpertGroupsItem from "./ExpertGroupsItem";
+import { DeleteConfirmationDialog } from "@common/dialogs/DeleteConfirmationDialog";
+import { t } from "i18next";
+import React, { useState } from "react";
+import { ICustomError } from "@utils/CustomError";
+import showToast from "@utils/toastError";
+import { useQueryDataContext } from "@common/QueryData";
+import { useQuery } from "@utils/useQuery";
+import { useServiceContext } from "@providers/ServiceProvider";
+import { TId } from "@/types";
 
 interface IExpertGroupsListProps {
   data: any;
@@ -9,17 +18,57 @@ interface IExpertGroupsListProps {
 const ExpertGroupsList = (props: IExpertGroupsListProps) => {
   const { data } = props;
   const { items = [] } = data;
+  const { query: fetchExpertGroups } = useQueryDataContext();
+  const { service } = useServiceContext();
+
+  const [openDeleteDialog, setOpenDeleteDialog] = useState<{
+    status: boolean;
+    id: TId;
+  }>({ status: false, id: "" });
+
+  const deleteExpertGroupQuery = useQuery({
+    service: (args, config) => service.expertGroups.info.remove(args, config),
+    runOnMount: false,
+    toastError: false,
+  });
+
+  const deleteExpertGroup = async () => {
+    try {
+      const {id} = openDeleteDialog
+      await deleteExpertGroupQuery.query({ id });
+      await fetchExpertGroups();
+    } catch (e) {
+      const err = e as ICustomError;
+      if (err.response?.data?.hasOwnProperty("message")) {
+        if (Array.isArray(err.response?.data?.message)) {
+          showToast(err.response?.data?.message[0]);
+        } else {
+          showToast(err);
+        }
+      }
+    }
+  };
   return (
     <Box mt={2}>
       <Grid container spacing={3}>
         {items?.map((expertGroup: any) => {
           return (
             <Grid item xs={12} sm={6} lg={4} key={expertGroup.id}>
-              <ExpertGroupsItem data={expertGroup} />
+              <ExpertGroupsItem data={expertGroup} setOpenDeleteDialog={setOpenDeleteDialog} />
             </Grid>
           );
         })}
       </Grid>
+      <DeleteConfirmationDialog
+        open={openDeleteDialog.status}
+        onClose={() =>
+          setOpenDeleteDialog({ ...openDeleteDialog, status: false })
+        }
+        onConfirm={deleteExpertGroup}
+        title="common.warning"
+        content="expertGroups.areYouSureYouWantDeleteExpertGroup"
+        confirmButtonText={t("common.continue")}
+      />
     </Box>
   );
 };
