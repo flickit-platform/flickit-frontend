d898653ac1318bfbfa0225ca051609c563fdc2a5
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-09-21 11:29:12 +0330

    FRO-644: Fix build error

--- src/components/common/dialogs/ContactUsDialog.tsx
+++ src/components/common/dialogs/ContactUsDialog.tsx
@@ -9,7 +9,7 @@ import { DialogProps } from "@mui/material/Dialog";
 import { useForm as useFormSpree } from "@formspree/react";
 import { FormProvider, useForm } from "react-hook-form";
 import { InputFieldUC } from "../fields/InputField";
-import telegramIcon from "@/assets/svg/telegram-icon.svg";
+import telegramIcon from "@/assets/svg/telegram-logo.svg";
 import splusIcon from "@/assets/svg/splus-logo.svg";
 import { styles } from "@styles";
 import { toast } from "react-toastify";
