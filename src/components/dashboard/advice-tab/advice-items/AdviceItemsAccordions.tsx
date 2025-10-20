3050b974c1b41ffcfe9773f8bbf2742c9b355c8c
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-10-20 14:15:38 +0330

    FRO-712: Fix: Remove questions

--- src/components/dashboard/advice-tab/advice-items/AdviceItemsAccordions.tsx
+++ src/components/dashboard/advice-tab/advice-items/AdviceItemsAccordions.tsx
@@ -471,7 +471,7 @@ const AdviceItemAccordion: React.FC<{
           );
           setDisplayedItems(updatedItems);
         }}
-        title={t("deleteItem")}
+        title={t("common.warning")}
         content={t("advice.deleteItemConfirmation", { title: item.title })}
       />
     </>
