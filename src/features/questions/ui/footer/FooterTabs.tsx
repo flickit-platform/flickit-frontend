14dc6532c1185a5c03e78d8f363d1c1381e1eb6a
e.mostafa <e.mostafa021@gmail.com>
2025-11-09 20:42:28 +0330

    fro-722: fix container

--- src/features/questions/ui/footer/FooterTabs.tsx
+++ src/features/questions/ui/footer/FooterTabs.tsx
@@ -8,19 +8,19 @@ import TabPanel from '@mui/lab/TabPanel';
 import { Text } from "@/components/common/Text";
 
 const FooterTabs = () => {
-  const { handleChange, selectedTab, tabItems, ActiveComponent,  } = useTabs();
-
+  const { handleChange, selectedTab, tabItems, ActiveComponent } = useTabs();
   return (
       <TabContext value={selectedTab}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             {tabItems.map((item: any) =>{
-              return  <Tab key={item.value} label={  <Text variant="bodyMedium"
-                                                           sx={{
-                                                             textTransform: "none",
-                                                             color: selectedTab === item.value ? "primary.main" : "background.secondaryDark",
-                                                             fontWeight: selectedTab === item.value ? 600 : 400,
-                                                           }}
+              return  <Tab key={item.value} label={
+                <Text variant="bodyMedium"
+                      sx={{
+                          textTransform: "none",
+                          color: selectedTab === item.value ? "primary.main" : "background.secondaryDark",
+                          fontWeight: selectedTab === item.value ? 600 : 400,
+                         }}
               >
                 {item.label}{"  "}({item.counts})
               </Text>} value={item.value}
